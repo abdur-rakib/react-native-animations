f65e7aec9769531b0610c2654411c79268cf8bb3
Abdur Rakib <abdurrakib961@gmail.com>
2023-01-23 18:11:31 +0600

    commented unused function

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
 import Home from './src/components/Home/Home';
 
 const App = () => {
-  const handleChange = () => {
-    console.log('Handle change function trigerred');
-  };
+  // const handleChange = () => {
+  //   console.log('Handle change function trigerred');
+  // };
+  // const handleChange2 = () => {
+  //   console.log('added another handle change function');
+  // };
   return (
     <View style={styles.container}>
       <Home />
