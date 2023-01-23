33f4171701e8c8197a267a5f36e602a3b38239db
Abdur Rakib <abdurrakib961@gmail.com>
2023-01-23 17:44:32 +0600

    add home comp

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
+import Home from './src/components/Home/Home';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <Home />
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
